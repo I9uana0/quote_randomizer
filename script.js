1ea4dcb97267fae0855b1c9b53b7e420ce7d0026
I9uana0 <ivchi06@gmail.com>
2025-07-04 18:10:50 +0300

    Добавлен минимальный html, создана функция получения цитаты

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-async function getRandomQuote(){
-  try{
-    const response = await fetch('https://api.quotable.io/random');
+getQuoteBtn.addEventListener("click", async () => {
+  await displayQuote();
+});
+
+async function getRandomQuote() {
+  try {
+    const response = await fetch("https://api.quotable.io/random");
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
     const data = await response.json();
     return data;
-  }catch(error){
-    console.error(`Ошибка при получении цитаты:`, error)
+  } catch (error) {
+    console.error(`Ошибка при получении цитаты:`, error);
     throw new Error(error);
   }
 }
 
+async function displayQuote() {
+  const quote = await getRandomQuote();
+  console.log(quote);
+  quoteContent.innerText = quote.content;
+  quoteAuthor.innerText = quote.author;
+}
+
 (async () => {
-    try {
-      const quote = await getRandomQuote();  // ждём результата
-      console.log(quote);                    // теперь это не Promise, а объект
-    } catch (err) {
-      console.error(err);
-    }
-  })();
+  try {
+    const quote = await getRandomQuote();
+    console.log(quote);
+    quoteContent.innerText = quote.content;
+    quoteAuthor.innerText = quote.author;
+  } catch (err) {
+    console.error(err);
+  }
+})();
